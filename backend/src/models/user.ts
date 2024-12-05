dc8201958f897338948bbce498ce8225c41035fa
Карпенко Глеб <glebkarpenko1@gmail.com>
2024-12-05 11:54:16 +0200

    add method for returning all user creditentials

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -12,7 +12,9 @@ const UserSchema = new mongoose.Schema({
 export const UserModel = mongoose.model('User', UserSchema);
 
 export const getUsers = () => UserModel.find();
-export const getUserByEmail = (email: String) => UserModel.findOne({ email });
+export const getUserByEmail = (email: String) => UserModel.findOne({ email })
+export const getUserCreditentialsByEmail = (email: String) => 
+    UserModel.findOne({ email }).select('+authentication.password +authentication.sessionToken');
 export const getUserBySessionToken = (sessionToken: string) => UserModel.findOne({
     'authentication.sessionToken': sessionToken,
 });
