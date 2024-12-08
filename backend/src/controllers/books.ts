83cfffe8aaf7fae749c7f0895a6cc9eafbd1ad7b
Карпенко Глеб <glebkarpenko1@gmail.com>
2024-12-08 20:38:04 +0200

    implement book adapters

--- backend/src/controllers/books.ts
+++ backend/src/controllers/books.ts
@@ -1,37 +1,6 @@
 import { Request, Response } from "express";
-
-import { annasArchiveClient, goodreadsApiClient } from "@app/services/apiClients";
-import { AnnasArchiveQuery, BooksData } from '@app/interfaces/Books'
-import {
-    validGoodreadsResponseData,
-    validAnnasResponseData,
-    mapGoodreadsBooks, 
-    getAnnasArchiveParams, 
-    mapAnnasArchiveBooks
-} from "@app/services/bookService";
-
-const fetchBooksGoodreads = async (searchQuery: string, pageNumber: number): Promise<BooksData> => {  
-    const options = {
-        method: 'GET',
-        url: `/searchBooks`,
-        params: {
-            keyword: searchQuery,
-            page: pageNumber
-        }
-    };
-
-    const response = await goodreadsApiClient.request(options);
-
-    if (!validGoodreadsResponseData(response)) {
-        throw new Error('Invalid Goodreads API response');
-    }
-
-    return {
-        books: mapGoodreadsBooks(response.data),
-        totalResults: response.data.totalResults || response.data.length,
-        currentPage: pageNumber
-    }
-}
+import { AnnasArchiveAdapter } from "@app/services/adapters/AnnasArchiveAdapter";
+import { GoodreadsAdapter } from "@app/services/adapters/GoodreadsAdapter";
 
 export const getGoodreadsBooks = async (req: Request, res: Response): Promise<void> => {
     const searchQuery = req.params.query;
@@ -43,7 +12,8 @@ export const getGoodreadsBooks = async (req: Request, res: Response): Promise<vo
     }
 
     try {
-        const goodreadsBookData = await fetchBooksGoodreads(searchQuery, pageNumber);
+        const adapter = new GoodreadsAdapter();
+        const goodreadsBookData = await adapter.fetchBooks(searchQuery, pageNumber);
         
         res.status(200).json(goodreadsBookData.books);
         return;
@@ -53,48 +23,21 @@ export const getGoodreadsBooks = async (req: Request, res: Response): Promise<vo
     }
 }
 
-const fetchBooksAnnasArchive = async (query: AnnasArchiveQuery): Promise<BooksData> => {
-    const params = getAnnasArchiveParams(query);
-
-    const options = {
-        method: 'GET',
-        url: `/search`,
-        params: params
-    };
-
-    const response = await annasArchiveClient.request(options);
-
-    if (!validAnnasResponseData(response)) {
-        throw new Error('Invalid Anna\'s Archive API response');
-    }
-
-    const totalResults = response.data.total
-
-    return {
-        books: mapAnnasArchiveBooks(response.data.books),
-        totalResults: totalResults || response.data.length,
-        currentPage: totalResults / 10
-    }
-}
-
 export const getAnnasArchiveBooks = async (req: Request, res: Response): Promise<void> => {
-    const searchQuery = req.params.query;
-    const author = req.params.author;
-    const categories = req.params.cat;
+    const params = {
+        query: req.params.query,
+        author: req.params.author,
+        category: req.params.cat
+    }
 
-    if (!searchQuery) {
+    if (!params.query) {
         res.status(400).json({ message: "Search query is required for Anna's Archive search." });
         return;
     }
 
-    const query: AnnasArchiveQuery = {
-        query: searchQuery,
-        author: author,
-        category: categories
-    }
-
     try {
-        const result = await fetchBooksAnnasArchive(query);
+        const adapter = new AnnasArchiveAdapter();
+        const result = await adapter.fetchBooks(params);
         res.status(200).json(result.books);
     } catch (error) {
         res.status(400).json({ message: "Could not fetch books from Anna's Archive."})
