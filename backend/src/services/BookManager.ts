112fd5bdc3419501d78402139daef73eb03ef2a0
Карпенко Глеб <glebkarpenko1@gmail.com>
2024-12-16 13:50:43 +0200

    move book-lookup logic to BookManager service

--- backend/src/services/BookManager.ts
+++ backend/src/services/BookManager.ts
@@ -2,6 +2,8 @@ import { BooksData, BookSources, DownloadInfo, ProjectGutenbergBook } from "@app
 import { IBookServiceAdapter } from "./adapters/IBookServiceAdapter";
 import { AnnasArchiveAdapter } from "./adapters/AnnasArchiveAdapter";
 import { GutenbergAdapter } from "./adapters/GutenbergAdapter";
+import { ClientBook, StorageBook } from "@app/interfaces/Books";
+import { linkExists, getBookById, addBookLinkProperty } from "@app/models/book";
 import { FileSizeMetric } from "@app/interfaces/Util";
 
 export class BookManager {
@@ -33,6 +35,94 @@ export class BookManager {
         }
 
         return adapter.searchBookById(id);
+    }
+
+    /**
+     * Looksup book details in the database. If some are missing, retrieves them from other sources like Project Gutenberg API.
+     * After retrieving from other sources saves them to database.
+     * @param id - local database book id.
+     * @returns - book with all details needed for client.
+     */
+    async lookupBook(id: string): Promise<ClientBook> {
+        const book = await getBookById(id);
+
+        if (linkExists(id) && book.link.complete) {
+            return this.mapBookforClient(book as StorageBook);
+        }
+
+        const resultBook = await this.formatBookDetails(id);
+        await this.extendBookData(resultBook);
+
+        return resultBook;
+    }
+
+    private async formatBookDetails(id: string): Promise<ClientBook> {
+        const book = (await getBookById(id)) as StorageBook;
+        const desiredFormat = 'epub';
+
+        const download = await this.getGutenbergDownloads(book.title, desiredFormat);
+        if (download.urls.length !== 0) {
+            book.link.downloadUrl = download.urls[0],
+            book.link.format = download.format || desiredFormat,
+            book.link.size.value = download.size.value || 0,
+            book.link.size.metric = download.size.metric || FileSizeMetric.Bytes
+        }
+
+        book.link.complete = false;
+
+        return this.mapBookforClient(book);
+    }
+
+    private async extendBookData(extendedBook: ClientBook): Promise<void>{
+        const existingBook = (await getBookById(extendedBook.id)) as StorageBook;
+
+        if (!existingBook) {
+            console.error(`Book with id ${extendedBook.id} does not exist.`);
+            return;
+        }
+
+        const linkData = this.mapBookForStorage(extendedBook);
+        try {
+            await addBookLinkProperty(existingBook.id, linkData);
+        } catch (error) {
+            console.error(`Could not extend book with id: ${extendedBook.id}`);
+        }
+    }
+
+    private mapBookforClient(book: StorageBook): ClientBook {        
+        return {
+            id: book.id,
+            title: book.title,
+            author: book.author,
+            genre: book.subject,
+            rating: book.rating,
+            publishedYear: book.publishedYear,
+            language: book.language,
+            link: {
+                coverUrl: book.coverUrl,
+                readUrl: book.link.readUrl,
+                downloadUrl: book.link.downloadUrl,
+                format: book.link.format,
+                size: {
+                    value: book.link.size.value,
+                    metric: book.link.size.metric
+                },
+                buyUrl: book.link.buyUrl
+            }
+        };
+    }
+
+    private mapBookForStorage(book: ClientBook): Partial<Record<string, any>> {
+        return {
+            readUrl: book.link.readUrl,
+            downloadUrl: book.link.downloadUrl,
+            format: book.link.format,
+            size: {
+                value: book.link.size.value,
+                metric: book.link.size.metric
+            },
+            buyUrl: book.link.buyUrl,
+        };
     }
 
     async getAnnasDownloads(title: string): Promise<DownloadInfo> {
@@ -81,7 +171,7 @@ export class BookManager {
      * @param desiredFormat - desired ebook download format as MIME type, for ex. 'epub' or 'mobi'.
      * @returns - calculated priority number, higher number means better resource.
      */
-    calculatePriority(resource: {uri: string, type: string}, desiredFormat: string): number {
+    private calculatePriority(resource: {uri: string, type: string}, desiredFormat: string): number {
         const containsImagesPriority = resource.uri.endsWith('images') ? 2 : 0;
 
         let desiredType = '';
