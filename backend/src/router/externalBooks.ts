5579491b42ec3466581bf20acdbb6decd9c46c1a
Карпенко Глеб <glebkarpenko1@gmail.com>
2024-12-19 10:42:35 +0200

    fix address typo

--- backend/src/router/externalBooks.ts
+++ backend/src/router/externalBooks.ts
@@ -15,7 +15,7 @@ export default (router: Router): void => {
     router.get('/books/annas/:query/:author/:cat', getAnnasArchiveBooks);
     router.get('/books/best/all/:genre', getBestBooksByGenre);
     router.get('/books/best/:id', searchBestBookById);
-    router.get('/books/open-lib/:q/:author/:cat/:acess/:lang', getOpenLibraryBooks);
+    router.get('/books/open-lib/:q/:author/:cat/:access/:lang', getOpenLibraryBooks);
     router.get('/books/gutenberg/all/:page', getAllGutenbergBooks);
     router.get('/books/gutenberg/:id', getGutenbergBook);
 }
