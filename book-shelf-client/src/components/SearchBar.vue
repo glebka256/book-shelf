83e9b09725ea4e614b2455bc0312c65c13e730b7
Карпенко Глеб <glebkarpenko1@gmail.com>
2024-12-20 10:57:20 +0200

    move search bar to its own component

---
+++ book-shelf-client/src/components/SearchBar.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+defineProps({
+  placeholder: {
+    type: String,
+    required: false,
+    default: "Search book"
+  }
+});
+</script>
+
+<template>
+ <div class="search-bar">
+  <i class="fas fa-search"></i> <input type="text" :placeholder=placeholder>
+ </div>
+</template>
+
+<style scoped lang="scss">
+.search-bar {
+  width: 40%;
+  position: relative;
+  top: -55px;
+  background-color: #9598d2;
+  padding: 40px;
+  border-radius: 0 0 60% 60%;
+
+  input {
+    width: 240px;
+    margin-top: 20px;
+    margin-bottom: -20px;
+    padding: 10px 15px;
+    border: none;
+    border-radius: 15px;
+    font-size: 1rem;
+    color: #2c3e50;
+    background-color: #f1f1f1;
+    outline: none;
+    transition: box-shadow 0.3s ease-in-out;
+  }
+
+  &::placeholder {
+      color: #b3b3b3;
+    }
+
+    &:focus {
+      box-shadow: 0 0 10px #7175d1;
+  }
+}
+
+@media (max-width: 1024px) {
+  .search-bar {
+    width: 80%;
+  }
+}
+
+@media (min-width: 1920px) {
+  .search-bar {
+    width: 50%;
+  }
+}
+</style>
--- book-shelf-client/src/views/HomeView.vue
+++ book-shelf-client/src/views/HomeView.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
 import HorizontalScroll from '@/components/HorizontalScroll.vue';
+import SearchBar from '@/components/SearchBar.vue';
 </script>
 
 <template>
  <div class="home-view">
-  <div class="search-bar">
-    <i class="fas fa-search"></i> <input type="text" placeholder="Search book">
-  </div>
+  <search-bar placeholder="Search book" />
   <div class="recommended-view">
     <div class="view-heading">
       <h2 class="view-title">Recommended</h2>
@@ -32,49 +31,6 @@ import HorizontalScroll from '@/components/HorizontalScroll.vue';
   display: flex;
   flex-direction: column;
   align-items: center;
-}
-
-.search-bar {
-  width: 40%;
-  position: relative;
-  top: -55px;
-  background-color: #9598d2;
-  padding: 40px;
-  border-radius: 0 0 60% 60%;
-
-  input {
-    width: 240px;
-    margin-top: 20px;
-    margin-bottom: -20px;
-    padding: 10px 15px;
-    border: none;
-    border-radius: 15px;
-    font-size: 1rem;
-    color: #2c3e50;
-    background-color: #f1f1f1;
-    outline: none;
-    transition: box-shadow 0.3s ease-in-out;
-  }
-
-  &::placeholder {
-      color: #b3b3b3;
-    }
-
-    &:focus {
-      box-shadow: 0 0 10px #7175d1;
-  }
-}
-
-@media (max-width: 1024px) {
-  .search-bar {
-    width: 80%;
-  }
-}
-
-@media (min-width: 1920px) {
-  .search-bar {
-    width: 50%;
-  }
 }
 
 .recommended-view, .search-view {
