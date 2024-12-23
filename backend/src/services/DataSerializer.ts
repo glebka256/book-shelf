87af73d0e7d376b56704efd8929ab41ce607c845
Карпенко Глеб <glebkarpenko1@gmail.com>
2024-12-23 08:53:08 +0200

    make serializer static

--- backend/src/services/DataSerializer.ts
+++ backend/src/services/DataSerializer.ts
@@ -3,19 +3,15 @@ import associations from "@app/data/associations.json";
 import { AssociateType, ScrapingTypes, SubjectAssociates } from "@app/interfaces/Data";
 
 export class DataSerializer {
-    private associates: SubjectAssociates[];
+    private static associates = Object.entries(associations).map(([key, value]) => ({
+        subject: {
+            name: key,
+            type: value.type as AssociateType,
+            associations: value.associates
+        }
+    }));
 
-    constructor() {
-        this.associates = Object.entries(associations).map(([key, value]) => ({
-            subject: {
-                name: key,
-                type: value.type as AssociateType,
-                associations: value.associates
-            }
-        }));
-    }
-
-    getParsingSubjects(type: ScrapingTypes ): string[] {
+    static getParsingSubjects(type: ScrapingTypes ): string[] {
         if (type === 'all') {
             return mainSubjects.concat(additionalSubjects);
         }
@@ -25,11 +21,11 @@ export class DataSerializer {
         return additionalSubjects
     }
 
-    getAllAsociates(): SubjectAssociates[] {
+    static getAllAsociates(): SubjectAssociates[] {
         return this.associates;
     }
 
-    getAssociations(name: string): SubjectAssociates {
+    static getAssociations(name: string): SubjectAssociates {
         const associate = this.associates.find(associate => associate.subject.name === name);
         if (!associate) {
             throw new Error(`Could not read subject associatoins from .JSON with name: ${name}`);
