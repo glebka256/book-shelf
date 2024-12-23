fef22e42d63763f10bd401333e6832e4bd1622dc
Карпенко Глеб <glebkarpenko1@gmail.com>
2024-12-23 08:34:08 +0200

    create subject matrix data serializer

---
+++ backend/src/interfaces/Data.ts
@@ -0,0 +1,19 @@
+export enum AssociateType {
+    Fiction = 'fiction',
+    NonFiction = 'non-fiction',
+    SelfHelp = 'self-help'
+}
+
+export interface SubjectAssociates {
+    subject: {
+        name: string,
+        type: AssociateType
+        associations: string[]
+    }
+}
+
+export enum ScrapingTypes {
+    Main = 'main',
+    Additional = 'additional',
+    All = 'all'
+}
---
+++ backend/src/services/DataSerializer.ts
@@ -0,0 +1,40 @@
+import { mainSubjects, additionalSubjects } from "@app/data/scrapingCategories.json";
+import associations from "@app/data/associations.json";
+import { AssociateType, ScrapingTypes, SubjectAssociates } from "@app/interfaces/Data";
+
+export class DataSerializer {
+    private associates: SubjectAssociates[];
+
+    constructor() {
+        this.associates = Object.entries(associations).map(([key, value]) => ({
+            subject: {
+                name: key,
+                type: value.type as AssociateType,
+                associations: value.associates
+            }
+        }));
+    }
+
+    getParsingSubjects(type: ScrapingTypes ): string[] {
+        if (type === 'all') {
+            return mainSubjects.concat(additionalSubjects);
+        }
+        if (type === 'main') {
+            return mainSubjects;
+        }
+        return additionalSubjects
+    }
+
+    getAllAsociates(): SubjectAssociates[] {
+        return this.associates;
+    }
+
+    getAssociations(name: string): SubjectAssociates {
+        const associate = this.associates.find(associate => associate.subject.name === name);
+        if (!associate) {
+            throw new Error(`Could not read subject associatoins from .JSON with name: ${name}`);
+        }
+
+        return associate;
+    }
+}
