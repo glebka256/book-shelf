853ec20e513c8d2183d9e6820f4bb16549f04610
Карпенко Глеб <glebkarpenko1@gmail.com>
2025-01-06 13:10:50 +0200

    connect logut page to server

--- book-shelf-client/src/views/LogoutView.vue
+++ book-shelf-client/src/views/LogoutView.vue
@@ -1,13 +1,53 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import baseInstance from '@/api/baseInstance';
+import AuthForm from '@/components/AuthForm.vue';
+import { FormField } from '@/types/Auth';
 
+const displayMessage = ref<boolean>(false);
+const message = ref<string>('');
+
+const logoutFields: FormField[] = [];
+
+function handleLogout(formData: Record<string, string>) {
+  logout(formData);
+}
+
+async function logout(formData: Record<string, string>) {
+  try {
+    const response = await baseInstance.post('auth/logout');
+
+    displayMessage.value = true;
+    message.value = `Logged out from account`;
+  } catch (error: any) {
+    displayMessage.value = true;
+    message.value = 'Could not connect to the server.';
+  }
+}
 </script>
 
 <template>
- <div class="">
-  <h1>This is Logout Component</h1>
+ <div class="logout-view">
+  <AuthForm
+    title="Exit you account"
+    :fields="logoutFields"
+    submit-text="Logout"
+    @submit="handleLogout"
+  >
+    <router-link to="/register">Want to create a new account? Register</router-link>
+  </AuthForm>
+  <div v-if="displayMessage" class="error">{{ message }}</div>
  </div>
 </template>
 
 <style scoped lang="scss">
-
+.logout-view {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  background: linear-gradient(135deg, #e3f2fd, #bbdefb);
+  font-family: 'Arial', sans-serif;
+}
 </style>
