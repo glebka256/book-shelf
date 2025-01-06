ed6df11d78ba11d6d369dcd1abd5d6899af103e3
Карпенко Глеб <glebkarpenko1@gmail.com>
2025-01-06 19:30:42 +0200

    use credentials to store cookies

--- book-shelf-client/src/api/baseInstance.ts
+++ book-shelf-client/src/api/baseInstance.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const baseInstance = axios.create({
-    baseURL: process.env.MAIN_API_BASE_URL || 'http://localhost:8080/'
+    baseURL: process.env.MAIN_API_BASE_URL || 'http://localhost:8080/',
+    withCredentials: true
 });
 
 export default baseInstance;
