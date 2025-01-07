fb675ae0dc05a47d3820828b8a9eab0d2dd5407a
Карпенко Глеб <glebkarpenko1@gmail.com>
2025-01-07 16:57:11 +0200

    create components with spinning loading animations

---
+++ book-shelf-client/src/components/TextLoader.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import LoadingSpinner from './LoadingSpinner.vue';
+
+const props = defineProps({
+  loaderText: {
+    type: String,
+    required: true
+  }
+});
+</script>
+
+<template>
+<div class="text-loader">
+    <LoadingSpinner />
+    <p>{{ props.loaderText }}</p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.text-loader {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: #666;
+
+  p {
+    font-size: 16px;
+    font-weight: 500;
+  }
+}
+</style>
