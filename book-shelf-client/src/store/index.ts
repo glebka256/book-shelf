21d7b855959db195b62d30c907de3dc583d730f5
Карпенко Глеб <glebkarpenko1@gmail.com>
2025-01-19 11:51:01 +0200

    create app store for user's favorite books

--- book-shelf-client/src/store/index.ts
+++ book-shelf-client/src/store/index.ts
@@ -1,9 +1,30 @@
-import { createStore } from "vuex";
+import { defineStore } from "pinia";
 
-export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+export const useFavoritesStore = defineStore('favorites', {
+  state: () => ({
+    favoriteBooksIds: [] as string[],
+  }),
+
+  getters: {
+    isFavorite: (state) => (id: string) => state.favoriteBooksIds.includes(id),
+    favoriteCount: (state) => state.favoriteBooksIds.length,
+  },
+
+  actions: {
+    addFavorite(id: string) {
+      if (!this.favoriteBooksIds.includes(id)) {
+        this.favoriteBooksIds.push(id);
+      }
+    },
+    removeFavorite(id: string) {
+      this.favoriteBooksIds.filter((itemId) => itemId !== id);
+    },
+    toggleFavorite(id: string) {
+      if (this.isFavorite(id)) {
+        this.removeFavorite(id);
+      } else {
+        this.addFavorite(id);
+      }
+    }
+  }
 });
