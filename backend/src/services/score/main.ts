d07bb401d9dee4f2b1bfc7dfeab70ebd181d6041
Карпенко Глеб <glebkarpenko1@gmail.com>
2025-01-28 21:59:09 +0200

    implement book scoring by chunks determined by genre

--- backend/src/services/score/main.ts
+++ backend/src/services/score/main.ts
@@ -1,10 +1,12 @@
 import dotenv from 'dotenv';
+import * as stringSimiliarity from 'string-similarity';
+import { countBy, maxBy } from 'lodash';
 import { connectDB } from '@app/config/db';
 import { StorageBook } from '@app/interfaces/Books';
 import { DataSerializer } from '../DataSerializer';
 import { getBooks } from '@app/models/book';
-import { ScrapingTypes } from '@app/interfaces/Data';
 import { saveRelations } from './serialization';
+import { toWhitespace } from '@app/utils';
 
 const WEIGHTS = {
     "title": 10,
@@ -23,6 +25,17 @@ interface ScoreBook {
     subjects: string[],
     year: number,
     rating: number
+}
+
+interface CategorizedGenres {
+    category: string[]
+    main: string[],
+    specific: string[]
+}
+
+interface ScoreBookChunk {
+    genre: string,
+    books: Set<ScoreBook>
 }
 
 export interface ScoreTable {
@@ -45,47 +58,30 @@ async function main() {
 
     console.log("Retrieving books from the DB...");
 
-    const subjects: string[] = DataSerializer.getParsingSubjects(ScrapingTypes.All);
-    const books: ScoreBook[] = (await getDBbooks()).slice(0, 100);  // slice for testing purposes
+    const books: ScoreBook[] = await getDBbooks();
+
+    console.log("Organizing books...");
+
+    const bookChunks: ScoreBookChunk[] = divideBooksByGenre(books);
 
     console.log("Scoring books...");
-    let completeTable: ScoreTable = {};
 
-    for (let i = 0; i < books.length; i++) {
-        for (let j = i; j < books.length; j++) {
-            const main = books[i];
-            const compared = books[j];
+    for (let i = 0; i < bookChunks.length; i++) {
+        const chunk: ScoreBookChunk = bookChunks[i];
 
-            if (!completeTable[main.id]) {
-                completeTable[main.id] = {};
-            }
-            if (!completeTable[compared.id]) {
-                completeTable[compared.id] = {};
-            }
+        console.log(`Processing chunk ${i} of genre: ${chunk.genre}`);
 
-            if (main.id !== compared.id) {
-                const score = scoreBooks(main, compared);
-                completeTable[main.id][compared.id] = score;
-                completeTable[compared.id][main.id] = score;
-            }
+        const chunkTable: ScoreTable = calculateScores(Array.from(chunk.books));
+        const chunkRecord: ScoreTableChunk = { 
+            subject: chunk.genre, 
+            chunk: String(i), 
+            data: chunkTable 
+        };
 
-            // Log for each thousand
-            const processedScores = i * books.length + j;
-            if (processedScores % 1000 === 999) {
-                console.log(`Calculated ${processedScores + 1} scores.`);
-            }
-        }
+        await saveRelations(chunkRecord);
     }
 
     console.log(`Processed ${books.length} books total.`);
-
-    const testChunk: ScoreTableChunk = {
-        subject: "test",
-        chunk: "test",
-        data: completeTable
-    }
-
-    await saveRelations(testChunk);
 
     process.exit(0);
 }
@@ -109,6 +105,130 @@ async function getDBbooks(): Promise<ScoreBook[]> {
     }));
 }
 
+/** Divides a list of books into chunks of unique books grouped by genre.
+ * Score books by chunks for optimal execution time
+ * 
+ * @param books - List of books of different genres to be grouped by
+ * @returns An array of chunks containing genre and unique books
+*/
+function divideBooksByGenre(books: ScoreBook[]): ScoreBookChunk[] {
+    const allGenres = retrieveGenres();
+    let result: ScoreBookChunk[] = [];
+
+    for (const book of books) {
+        const genre = getBookGenre(book.subjects, allGenres);
+
+        let chunk = result.find(chunk => chunk.genre === genre);
+
+        if (!chunk) {
+            chunk = { genre: genre, books: new Set<ScoreBook> };
+            result.push(chunk);
+        } else {
+            chunk.books.add(book);
+        }
+    }
+
+    return result;
+}
+
+function retrieveGenres(): CategorizedGenres {
+    const allGenres = DataSerializer.parseGenres();
+
+    const categories: string[] = ['fiction', 'non-fiction', 'self-help'];
+    let mainGenres: string[] = [];
+    let specificGenres: string[] = [];
+
+    for (const genre of allGenres) {
+        mainGenres.push(toWhitespace(genre.name));
+        
+        let excludingMain = genre.subjects.filter(subject => subject !== genre.name);
+        excludingMain = excludingMain.map((genre) => toWhitespace(genre));
+        specificGenres = specificGenres.concat(excludingMain);
+    }
+
+    return { category: categories, main: mainGenres, specific: specificGenres };
+}
+
+/**
+ * Determines most relavant book subject to be chosen as its genre.
+ * If a subject can be a genre is determined by a list of possible genres in the system.
+ * Priority is given to most frequent subject or the one that is less popular, in order to differentiate.
+ * 'uncategorized' is used as fallback.
+ * 
+ * @param bookSubjects - An array of subjects associated with book
+ * @returns Most relevant subject that could serve as genre
+ */
+function getBookGenre(bookSubjects: string[], genres: CategorizedGenres): string {
+    bookSubjects = bookSubjects.map((subject) => subject.toLowerCase());
+    const genreTypes = [genres.specific, genres.main, genres.category];
+
+    for (const genre of genreTypes) {
+        const result = findClosestGenre(bookSubjects, genre);
+        if (result) {
+            return result;
+        }
+    }
+
+    return getMostFrequent(bookSubjects);
+}
+
+function findClosestGenre(subjects: string[], genres: string[]): string | undefined {
+    let bestMatch: string | undefined;
+    let bestMatchScore = 0;
+
+    for (const subject of subjects) {
+        const match = stringSimiliarity.findBestMatch(subject, genres);
+
+        if (match.bestMatch.rating > bestMatchScore) {
+            bestMatchScore = match.bestMatch.rating;
+            bestMatch = match.bestMatch.target;
+        }
+    }
+
+    return bestMatch;
+}
+
+function getMostFrequent(array: string[]): string {
+    const frequency = countBy(array);
+
+    if (Math.max(...Object.values(frequency)) > 1) {
+        const mostFrequent = maxBy(Object.keys(frequency), o => frequency[o]);
+        return mostFrequent;
+    }
+
+    return array.at(-1);
+}
+
+/**
+ * Creates a table where key is book id and values are it's similiarity score 
+ * with other books of the group.
+ */
+function calculateScores(books: ScoreBook[]): ScoreTable {
+    let completeTable: ScoreTable = {};
+
+    for (let i = 0; i < books.length; i++) {
+        for (let j = i; j < books.length; j++) {
+            const main = books[i];
+            const compared = books[j];
+
+            if (!completeTable[main.id]) {
+                completeTable[main.id] = {};
+            }
+            if (!completeTable[compared.id]) {
+                completeTable[compared.id] = {};
+            }
+
+            if (main.id !== compared.id) {
+                const score = scoreBooks(main, compared);
+                completeTable[main.id][compared.id] = score;
+                completeTable[compared.id][main.id] = score;
+            }
+        }
+    }
+
+    return completeTable;
+}
+
 function scoreBooks(book1: ScoreBook, book2: ScoreBook): number {
     let score = 0;
 
@@ -130,7 +250,7 @@ function scoreArray(array1: string[], array2: string[], weight: number): number 
     const maximumMatches = Math.max(array1.length, array2.length);
     
     // Normalizes score by the maximim number of possible mathces
-    // This should prevent significant overlap for books with many subjects.
+    // This should prevent significant overlap for books with many subjects or authors.
     return (matches.length / maximumMatches) * weight;
 }
 
