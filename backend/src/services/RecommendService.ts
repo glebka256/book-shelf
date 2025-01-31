6ea9ec465b2d67116d59ad02097d3e670aea50b9
Карпенко Глеб <glebkarpenko1@gmail.com>
2025-01-31 16:22:00 +0200

    implement basic recommendation forming

--- backend/src/services/RecommendService.ts
+++ backend/src/services/RecommendService.ts
@@ -1,10 +1,12 @@
 import { StorageBook } from "@app/interfaces/Books";
-import { UserInteraction } from "@app/interfaces/User";
+import { interactionTypes, UserInteraction } from "@app/interfaces/User";
 import { Languages } from "@app/interfaces/Util";
-import { getBooks } from "@app/models/book";
-import { getUserInteractions, getUserWithFavoritesIds} from "@app/models/user";
-import { extractBookFromDoc } from "@app/utils";
+import { getBookById, getBooks, queryBooks } from "@app/models/book";
+import { getUserWithFavoritesIds} from "@app/models/user";
+import { extractBookFromDoc, sliceMap, sortNumericRecord } from "@app/utils";
 import { InteractionService } from "./InteractionService";
+import { getBookGenre, retrieveGenres } from "@app/services/genreService";
+import { loadBookRelationsSubject } from "./score/serialization";
 
 export class RecommendService {
     private user: RecommendationUser;
@@ -16,8 +18,9 @@ export class RecommendService {
         this.preferedLanuages = languages;
     }
 
-    setUser(userId: string) {
+    async setUser(userId: string) {
         this.user = new RecommendationUser(userId);
+        await this.user.init();
     }
 
     /**
@@ -41,6 +44,33 @@ export class RecommendService {
 
         return scoredBooks.slice(skip, skip + limit);
     }
+
+    async formRecommendations(limit: number): Promise<StorageBook[]> {
+        let recommedationIds = await this.getRecommendedIds();
+
+        recommedationIds = sliceMap(recommedationIds, limit);
+        const aggregatedIds = Array.from(recommedationIds.values()).flat();
+
+        const books = await queryBooks({ _id: { $in: aggregatedIds } });
+        return books as StorageBook[];
+    }
+
+    async getRecommendedIds(limit: number = 10): Promise<Map<string, string[]>> {
+        const userInteractions = this.user.getAllInteractions();
+        const recommedationIds = new Map<string, string[]>();
+        const allGenres = retrieveGenres();
+
+        for (const interaction of userInteractions) {
+            const book = await getBookById(interaction.bookId);
+            const genre = getBookGenre(book.subject, allGenres);
+            const relatives = await loadBookRelationsSubject(genre, book.id);
+
+            const sortedRelatedIds = Object.keys(sortNumericRecord(relatives));
+            recommedationIds.set(book.id, sortedRelatedIds.slice(limit));
+        }
+
+        return recommedationIds;
+    }
 }
 
 class RecommendationUser {
@@ -50,8 +80,11 @@ class RecommendationUser {
 
     constructor(userID: string) {
         this.id = userID;
-        this.retrieveFavorites();
-        this.retrieveInteractions();
+    }
+
+    async init(): Promise<void> {
+        await this.retrieveFavorites();
+        await this.retrieveInteractions();
     }
 
     async retrieveFavorites(): Promise<string[]> {
@@ -62,10 +95,23 @@ class RecommendationUser {
     }
 
     async retrieveInteractions(): Promise<UserInteraction[]> {
-        const user = await getUserInteractions(this.id);
         const interactionManager = new InteractionService(this.id);
-        this.interactions = interactionManager.mapInteraction(user.interactions);
+        this.interactions = await interactionManager.load();
 
         return this.interactions;
+    }
+
+    getAllInteractions(): UserInteraction[] {
+        let favoritesInteractions: UserInteraction[] = [];
+
+        for (const favoriteId of this.favoritesIds) {
+            favoritesInteractions.push({ 
+                type: interactionTypes.favorite, 
+                bookId: favoriteId,
+                timestamp: new Date() // Date is set with latest in order to have highest time priority for items that are favorite now.
+            });
+        }
+
+        return [...favoritesInteractions, ...this.interactions];
     }
 }
