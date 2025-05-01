3965f7b1daa914fc03a79581a1c2ed53502e0c5f
Карпенко Глеб <glebkarpenko1@gmail.com>
2025-05-01 10:09:45 +0300

    initial setup

--- admin-shelf/src/views/AboutView.vue
+++ admin-shelf/src/views/AboutView.vue
@@ -1,5 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+ <div class="">
+  <h1>This is AboutView Component</h1>
+ </div>
 </template>
+
+<style scoped lang="scss">
+
+</style>
