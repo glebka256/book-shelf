f72ec9784ca1efbc5d376426bc7eba9dff07899c
Карпенко Глеб <glebkarpenko1@gmail.com>
2025-05-06 19:41:32 +0300

    create base axios instance for server api

---
+++ admin-shelf/src/config/axios.ts
@@ -0,0 +1,40 @@
+import axios, { AxiosError } from "axios";
+
+const baseInstance = axios.create({
+    baseURL: process.env.MAIN_API_BASE_URL || 'http://localhost:8080/',
+    withCredentials: true
+});
+
+baseInstance.interceptors.response.use(
+    response => response,
+    (error: AxiosError) => {
+        // Centralized error handling
+        if (error.response) {
+            // Server responded with a status code outside 2xx
+            switch (error.response.status) {
+            case 400:
+                alert('Bad Request:' + error.response.data);
+                break;
+            case 401:
+                alert('Unauthorized. Maybe redirect to login?');
+                break;
+            case 500:
+                alert('Internal Server Error');
+                break;
+            default:
+                alert('Unhandled error:' + error.response);
+            }
+        } else if (error.request) {
+            // No response received
+            alert('Network error or server not reachable');
+        } else {
+            // Something happened in setting up the request
+            alert('Axios error:' + error.message);
+        }
+    
+        // Return a rejected Promise to allow further handling if needed
+        return Promise.reject(error);
+    }
+);
+
+export default baseInstance;
