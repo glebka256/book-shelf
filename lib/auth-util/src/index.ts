76371f602dc229a22729289c122885ef3053af75
Карпенко Глеб <glebkarpenko1@gmail.com>
2025-05-12 12:09:21 +0300

    change package name, provide api interface as well

--- lib/auth-util/src/index.ts
+++ lib/auth-util/src/index.ts
@@ -3,9 +3,12 @@ import { AuthAPI, useAuthAPI } from "./auth.api";
 import { AuthService, useAuthService } from "./authService";
 
 /** Provide axios instance of api on which authentication will run */
-export function useAuth(baseInstance: AxiosInstance): AuthService {
+export function useAuth(baseInstance: AxiosInstance): { api: AuthAPI, service: AuthService } {
     const api: AuthAPI = useAuthAPI(baseInstance);
     const service: AuthService = useAuthService(api);
 
-    return service;
+    return {
+        api,
+        service
+    };
 }
