b7412a26d463cec15abf430c6fad1300d855a5f4
Карпенко Глеб <glebkarpenko1@gmail.com>
2025-05-12 19:15:35 +0300

    add logout/delete actions to AccountView

--- lib/client-shelf/src/views/AccountView.vue
+++ lib/client-shelf/src/views/AccountView.vue
@@ -1,10 +1,42 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
-import { User } from '@/types/Auth';
+import { useRouter } from 'vue-router';
 import auth from '@/config/auth';
+import { User } from '@book-shelf/auth-util/src/auth.types';
+import ActionButton from '@/../../component-lib/src/components/buttons/ActionButton.vue';
 
 const user = ref<User>();
 const errorMessage = ref<string>();
+const router = useRouter();
+
+const handleLogout = async () => {
+  const logoutConfirmed = confirm("Are you sure you want to exit from account?");
+  if (logoutConfirmed) {
+    const result = await auth.api.requestLogout();
+    if (result.status) {
+      alert("Successfully loged out from account");
+      router.push('/');
+    } else {
+      alert(result.message);
+      errorMessage.value = result.message;
+    }
+  }
+}
+
+const handleDelete = async () => {
+  const deleteConfirmed = confirm("Are you sure you want to delete this account?");
+  if (deleteConfirmed) {
+    const result = await auth.api.requestDelete();
+    if (result.status) {
+      alert("Account deleted");
+      auth.api.requestLogout();
+      router.push('/');
+    } else {
+      alert(result.message);
+      errorMessage.value = result.message;
+    }
+  }
+}
 
 onMounted(async () => {
   const response = await auth.api.getUserCredentials();
@@ -21,11 +53,16 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="account-view">
+  <div class="profile-manager">
    <h2>Account Information</h2>
    <div v-if="user" class="credentials">
      <p><strong>Username:</strong> {{ user.username }}</p>
      <p><strong>Email:</strong> {{ user.email }}</p>
+
+     <div class="button-group">
+      <ActionButton type="button" text="Logout" class="btn-secondary" @click="handleLogout" />
+      <ActionButton type="button" text="Delete Account" class="btn-danger" @click="handleDelete" />
+     </div>
    </div>
    <div v-else-if="errorMessage" class="error">
      <p>{{ errorMessage }}</p>
@@ -38,7 +75,7 @@ onMounted(async () => {
 </template>
 
 <style scoped lang="scss">
-.account-view {
+.profile-manager {
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -46,6 +83,13 @@ onMounted(async () => {
   min-height: 100vh;
   background: linear-gradient(135deg, #e3f2fd, #bbdefb);
   font-family: 'Arial', sans-serif;
+}
+
+.button-group {
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+  margin-top: 1rem;
 }
 
 .credentials, .not-signed-in, .error {
