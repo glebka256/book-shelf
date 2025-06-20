f7c86c81372632492ce10d3198888e8ee3efcdc1
Карпенко Глеб <glebkarpenko1@gmail.com>
2025-06-20 18:42:00 +0300

    specify one entry point for building and running all apps

--- lib/backend/src/index.ts
+++ lib/backend/src/index.ts
@@ -1,5 +1,16 @@
 import express, { ErrorRequestHandler } from 'express';
 import https from 'http';
+
+// Register path alias configuration
+import path from 'path';
+import { register } from 'tsconfig-paths';
+import tsConfig from '../tsconfig.json'; 
+
+register({
+  baseUrl: path.resolve(__dirname),
+  paths: tsConfig.compilerOptions.paths as any
+});
+
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
@@ -11,39 +22,61 @@ import router from '@app/router';
 import { Logger } from './utils/Logger';
 import { errorHandler } from './middlewares/errors';
 
+dotenv.config();
+
 Logger.enableDebug();
 
 const app = express();
 
-const allowedOrigins = ['http://localhost:8081', 'http://localhost:8085'];
 
+// CORS
+const allowedOrigins = ['http://localhost:8080', 'http://localhost:8081', 'http://localhost:8085'];
 app.use(cors({
     origin: function (origin, callback) {
         if (!origin || allowedOrigins.includes(origin)) {
             callback(null, true);
         } else {
-            callback(new Error('Origin not allowed by CORS'));
+            callback(new Error('Origin not allowed by CORS: ' + origin));
         }
     },
     credentials: true,
 }));
 
+
 app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
 
-const server = https.createServer(app);
 
-dotenv.config();
+// Serve client Vue app
+const clientPath = path.join(__dirname, '../../client-shelf/dist');
+app.use('/', express.static(clientPath));
+
+// Serve admin Vue app
+const adminPath = path.join(__dirname, '../../admin-shelf/dist');
+app.use('/admin', express.static(adminPath));
+
+// Serve webAPI app
+app.use('/api', router());
+
+// SPA Fallbacks
+app.get('/admin/*', (_req, res) => {
+    res.sendFile(path.join(adminPath, 'index.html'));
+});
+
+app.get('/*', (_req, res) => {
+    res.sendFile(path.join(clientPath, 'index.html'));
+});
+
+
+app.use(errorHandler as ErrorRequestHandler);
 
 connectDB();
 
 const port = process.env.PORT;
 const host = process.env.HOST;
 
+const server = https.createServer(app);
 server.listen(port, () => {
     Logger.info(`Server running on ${host}`, "APP");
 });
-
-app.use('/', router());
-app.use(errorHandler as ErrorRequestHandler);
--- lib/client-shelf/src/api/baseInstance.ts
+++ lib/client-shelf/src/api/baseInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseInstance = axios.create({
-    baseURL: process.env.MAIN_API_BASE_URL || 'http://localhost:8080/',
+    baseURL: process.env.MAIN_API_BASE_URL || 'http://localhost:8080/api/',
     withCredentials: true
 });
 
